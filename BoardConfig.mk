634bfe2826f00cce8319c3cd302142554e8e3470
ArianK16a <ArianK16a@users.noreply.github.com>
2021-10-07 16:42:50 +0530

    rosy: sepolicy: Switch to minimal-sepolicy
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,9 +159,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
