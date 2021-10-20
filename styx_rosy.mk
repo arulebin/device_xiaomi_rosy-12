dfc8d27f80b87264d97af3543f9409820416ebcb
arulebin <ebinarul@protonmail.com>
2021-10-20 12:08:37 +0000

    rosy: styxos bringups

--- styx_rosy.mk
+++ styx_rosy.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common stuff
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+$(call inherit-product, vendor/styx/config/common.mk)
 
 # Inherit from rosy device
 $(call inherit-product, device/xiaomi/rosy/device.mk)
